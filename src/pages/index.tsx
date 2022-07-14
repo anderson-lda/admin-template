65ab7d498c3f37343bb9774f649aca76380c68d9
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-07-14 16:33:43 -0300

    adjusted tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className={`h-screen bg-gradient-to-r from-green-500 to-blue-500`}>
+      <h1 className="text-3xl font-bold underline">Admin</h1>
+    </div>
+  )
+}
