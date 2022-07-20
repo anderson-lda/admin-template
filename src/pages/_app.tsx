33aae126b4639b874bffd46fcf9ae57f9cb75606
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-07-20 11:20:14 -0300

    with context api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import { AppProvider } from '../data/context/AppContext'
 
+//provider esta provendo para todos os objetos da aplicacao
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>)
 }
 
 export default MyApp
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,10 +1,14 @@
 import Layout from "../components/template/Layout"
+import useAppData from "../data/hook/UseAppData"
 
 export default function Notificaoes() {
+  const {alternarTema} = useAppData()
+
   return (
     <Layout titulo="Notificações" 
     subtitulo="Aqui você irá gerenciar suas notificações">
       <h3>conteudo</h3>
+      <button onClick={alternarTema}>Alternar Tema</button>
     </Layout>
   )
 }
