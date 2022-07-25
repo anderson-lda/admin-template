8ca3e49ff93430700f3b1715f30be23ebc7f427b
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-07-25 10:13:26 -0300

    authentication screen finished

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,14 +1,11 @@
 import Layout from "../components/template/Layout"
-import useAppData from "../data/hook/UseAppData"
 
 export default function Notificaoes() {
-  const {alternarTema} = useAppData()
 
   return (
     <Layout titulo="Notificações" 
     subtitulo="Aqui você irá gerenciar suas notificações">
       <h3>conteudo</h3>
-      <button onClick={alternarTema}>Alternar Tema</button>
     </Layout>
   )
 }
